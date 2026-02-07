3eeeba523f36a7a6063e55fac9ae592d61299af5
Chrisss903 <christopher.r@softsuave.org>
2026-02-07 15:57:39 +0530

    added secondary color

---
+++ src/components/DownloadAppSection.tsx
@@ -0,0 +1,13 @@
+const DownloadAppSection = () => {
+  return (
+    <div className="h-244.75 mx-[10%]">
+      <div>
+        <span className="text-4 text-secondary font-semibold leading-1.5 tracking-[0] uppercase">
+          Download App
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export { DownloadAppSection };
