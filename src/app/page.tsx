bba99efe4bb7d32ab24d11d078dff969e893ac25
Mathan902 <mathanmss902@gmail.com>
2026-02-07 15:22:49 +0530

    add additional section to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <div className="bg-red-200 h-[300px]"></div>
         <div className="bg-red-200 h-[300px]"></div>
         <div className="bg-red-200 h-[300px]"></div>
+        <div className="bg-red-200 h-[300px]"></div>
         <Features />
       </section>
     </>
