31fc823d80804ac4b2805765a93437f90414fd26
Chrisss903 <christopher.r@softsuave.org>
2026-02-07 13:14:20 +0530

    integrate font family for project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
   subsets: ["latin"],
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${urbanist.variable} font-sans antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-<>
-<h1 className="!text-red-500 bg-white ml-[70px] text-center hover:!text-blue-500">No klnlkiohg;</h1></>
+    <>
+      <h1 className="!text-red-500 bg-white ml-[70px] text-center hover:!text-blue-500">
+        No klnlkiohg;
+      </h1>
+    </>
   );
 }
